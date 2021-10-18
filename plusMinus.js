abfa0cdbf437c2f08e721e78df0c54492f024957
GraceLR <gracerwork08@gmail.com>
2021-10-18 02:04:11 +0000

    plusMinus.js

---
+++ plusMinus.js
@@ -0,0 +1,19 @@
+
+
+function plusMinus(arr) {
+    let pos = 0;
+    let neg = 0;
+    let zero = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            pos += 1;
+        } else if (arr[i] < 0) {
+            neg += 1;
+        } else if (arr[i] === 0) {
+            zero += 1;
+        }
+    }
+     console.log((pos/arr.length).toFixed(6));
+     console.log((neg/arr.length ).toFixed(6));
+     console.log((zero/arr.length).toFixed(6));
+}
