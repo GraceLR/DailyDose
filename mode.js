dc65564ab87f7a5b9d728c3f58890464044c3671
GraceLR <gracerwork08@gmail.com>
2021-11-06 20:42:35 +0000

    mode recursion

--- mode.js
+++ mode.js
@@ -1,26 +1,36 @@
 
 
 const mode = function(arr) {
-
+    
   let newArray = arr.sort(function(a, b) {
     return a - b;
   });
-  
-  let count = 1;
-  let num = newArray[0];
-  let currCount = 1;
-  
-  for (let i = 0; i < newArray.length - 1; i++) {
-    if (newArray[i] === newArray[i + 1]) {
-      currCount += 1;
-    } else {
-      if (currCount > count) {
-        count = currCount;
-        num = newArray[i];
-      }
-      currCount = 1;
+
+  let num = undefined;
+  const recurse = (array) => {
+    if (array.length === 0) {
+      return 0;
     }
-  }
+
+    let count = 0;
+    let i = 0;
+    while (array[i] === array[0] && i < array.length) {
+      count += 1;
+      i += 1;
+    }
+
+    const val = recurse(array.slice(i));
+
+    if (val > count) {
+      return val;
+    } else {
+      num = array[0];
+      return count;
+    }
+
+  };
+
+  recurse(newArray);
   
   return num;
   
