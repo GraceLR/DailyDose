b08ac7b4e39f5e100611f9e82242b13ed0393df5
GraceLR <gracerwork08@gmail.com>
2021-11-06 20:43:29 +0000

    mode dictionary

--- mode.js
+++ mode.js
@@ -1,37 +1,24 @@
 
 
 const mode = function(arr) {
-    
-  let newArray = arr.sort(function(a, b) {
-    return a - b;
-  });
 
-  let num = undefined;
-  const recurse = (array) => {
-    if (array.length === 0) {
-      return 0;
-    }
-
-    let count = 0;
-    let i = 0;
-    while (array[i] === array[0] && i < array.length) {
-      count += 1;
-      i += 1;
-    }
-
-    const val = recurse(array.slice(i));
-
-    if (val > count) {
-      return val;
-    } else {
-      num = array[0];
-      return count;
-    }
-
-  };
-
-  recurse(newArray);
+  const dict = {};
+  let num;
+  let maxCount = 0;
   
+  arr.forEach(n => {
+    if (dict[n] === undefined) {
+      dict[n] = 1;
+    } else {
+      dict[n] += 1;
+    }
+  
+    if (dict[n] > maxCount) {
+      maxCount = dict[n];
+      num = n;
+    }
+  });
+    
   return num;
   
 };
