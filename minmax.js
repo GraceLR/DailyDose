97394282a0f93ef5a8ea2ceb3513626ae9d06eac
GraceLR <gracerwork08@gmail.com>
2021-11-12 21:16:51 +0000

    minmax

---
+++ minmax.js
@@ -0,0 +1,20 @@
+
+const minmax = function(list) {
+
+    if (list === []) {
+      return [undefined, undefined];
+    } else {
+      let min = list[0];
+      let max = list[0];
+      list.forEach(ele => {
+        if (ele > max) {
+          max = ele;
+        }
+        if (ele < min) {
+          min = ele;
+        }
+      });
+      return [min, max];
+    }
+  
+  };
