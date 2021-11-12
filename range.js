9c25315a57815f632c0d6f4e2c7d359dc5a55236
GraceLR <gracerwork08@gmail.com>
2021-11-12 21:15:41 +0000

    range

---
+++ range.js
@@ -0,0 +1,24 @@
+
+const range = function(count, skipZero, descending) {
+
+    if (typeof count !== 'number') {
+      return [];
+    }
+  
+    let arr = [];
+  
+    if (descending === true) {
+      let num = skipZero === true ? count : count - 1;
+      for (let i = 0; i < count; i++) {
+        arr.push(num);
+        num -= 1;
+      }
+    } else {
+      let num = skipZero === true ? 1 : 0;
+      for (let i = 0; i < count; i++) {
+        arr.push(num);
+        num += 1;
+      }
+    }
+    return arr;
+  };
