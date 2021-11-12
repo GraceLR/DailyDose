e6abb0fb80bc8a948f341be69ca07bb291b5e188
GraceLR <gracerwork08@gmail.com>
2021-11-12 21:21:51 +0000

    tempConverter

---
+++ tempConverter.js
@@ -0,0 +1,13 @@
+
+
+const tempConverter = function(value, cToF) {
+
+    if (typeof value !== 'number') {
+      return NaN;
+    } else if (cToF === true) {
+      return Number((value * 9 / 5) + 32);
+    } else if (cToF === false) {
+      return Number((value - 32) * 5 / 9).toFixed(1);
+    }
+  
+  };
