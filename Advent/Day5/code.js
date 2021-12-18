66686ff424fc98a3bb89078c35f173b98d58be29
GraceLR <gracerwork08@gmail.com>
2021-12-18 02:40:47 +0000

    Advant Day5 both parts

--- Advent/Day5/code.js
+++ Advent/Day5/code.js
@@ -2,13 +2,15 @@
 const { test, real } = require("./input");
 
 const compare = (num1, num2) => {
-  return num1 > num2 ? { max: num1, min: num2} : { max: num2, min: num1};
+  return num1 > num2 ? { max: num1, min: num2, indexMax: 0, indexMin: 1} : 
+  { max: num2, min: num1,  indexMax: 1, indexMin: 0};
 };
 
-const day5Part1 = input => {
+const day5 = input => {
   let matrix = [];
   let count = 0;
   input.forEach((line, index) => {
+    // console.log(index + '  begin ------')
     if(line[0] === line[2]) {
       const max = compare(Number(line[1]), Number(line[3])).max;
       const min = compare(Number(line[1]), Number(line[3])).min;
@@ -43,16 +45,57 @@ const day5Part1 = input => {
         }
       }
     }
-    // console.log(index + '------')
+
+    // diaganal???
+ 
+    if(line[2] - line[0] === line[3] - line[1]) {
+      const max = compare(Number(line[1]), Number(line[3])).max;
+      const min = compare(Number(line[1]), Number(line[3])).min;
+      const indexMin = compare(Number(line[1]), Number(line[3])).indexMin;
+      let j = [Number(line[0]), Number(line[2])][indexMin];
+      for(let i = min; i <= max; i++) {
+        if(matrix[i] === undefined) {
+          matrix[i] = [];
+        }
+        if(matrix[i][j] === undefined) {
+          matrix[i][j] = 1;
+        } else {
+          matrix[i][j] += 1;
+          if(matrix[i][j] === 2) {
+            count += 1;
+          }
+        }
+        j += 1;
+      }
+    }
+    if(line[2] - line[0] === -(line[3] - line[1])) {
+      const max = compare(Number(line[1]), Number(line[3])).max;
+      const min = compare(Number(line[1]), Number(line[3])).min;
+      const indexMin = compare(Number(line[1]), Number(line[3])).indexMin;
+      let j = [Number(line[0]), Number(line[2])][indexMin];
+      for(let i = min; i <= max; i++) {
+        if(matrix[i] === undefined) {
+          matrix[i] = [];
+        }
+        if(matrix[i][j] === undefined) {
+          matrix[i][j] = 1;
+        } else {
+          matrix[i][j] += 1;
+          if(matrix[i][j] === 2) {
+            count += 1;
+          }
+        }
+        // console.log(i)
+        // console.log(j)
+        j -= 1;
+      }
+    }
+
+    // console.log(index + '  end ------')
     // console.log(matrix)
   });
   return count;
 }
 
-console.log(day5Part1(test));
-console.log(day5Part1(real));
-
-
-
-// console.log(day5Part2(testDraw, testBoards));
-// console.log(day5Part2(realDraw, realBoards));
+console.log(day5(test));
+console.log(day5(real));
