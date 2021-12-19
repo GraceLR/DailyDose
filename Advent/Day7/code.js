8244ac22024976ed7cd4d5d6c114098036bad0e0
GraceLR <gracerwork08@gmail.com>
2021-12-19 18:40:20 +0000

    Advant Day7 part2

--- Advent/Day7/code.js
+++ Advent/Day7/code.js
@@ -7,26 +7,53 @@ const sum = arr => {
   return sum;
 };
 
-const min = input => {
+const sumN = n => {
+  return (1 + n) * n / 2;
+}
+
+const minMax = input => {
   let min = input[0];
+  let max = input[0];
   input.forEach(num => {
     if(num < min) {
       min = num;
     }
+    if(num > max) {
+      max = num;
+    }
   });
-  return min;
+  return { min, max };
 };
 
-const day7Part1 = input => {
-  const cost = input.map(pos => {
+// const day7Part1 = input => {
+//   const cost = input.map(pos => {
+//     return input.map(ele => {
+//       return ele - pos < 0 ? -(ele - pos) : ele - pos;
+//     });
+//   });
+//   return min(cost.map(ele => sum(ele)));
+// }
+
+// console.log(day7Part1(test));
+// console.log(day7Part1(real));
+
+const day7Part2 = input => {
+  const position = [];
+  let num = minMax(input).min;
+  for(let i = 0; i <= minMax(input).max - minMax(input).min; i++) {
+    position.push(num);
+    num += 1;
+  }
+  const cost = position.map(pos => {
     return input.map(ele => {
-      return ele - pos < 0 ? -(ele - pos) : ele - pos;
+      return ele - pos < 0 ? sumN(-(ele - pos)) : sumN(ele - pos);
     });
   });
-  return min(cost.map(ele => sum(ele)));
+  // console.log(cost)
+  return minMax(cost.map(ele => sum(ele))).min;
 }
 
-console.log(day7Part1(test));
-console.log(day7Part1(real));
+console.log(day7Part2(test));
+console.log(day7Part2(real));
 
 
