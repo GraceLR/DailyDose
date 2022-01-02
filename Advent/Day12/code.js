67d36f45b67188854dddf5ff1dd8b1e6a222668a
GraceLR <gracerwork08@gmail.com>
2022-01-02 05:53:05 +0000

    Advent day12 part1

---
+++ Advent/Day12/code.js
@@ -0,0 +1,110 @@
+
+const { test, real } = require("./input");
+
+// func1(0);
+
+// const func1 = x => {
+//   x = x + 1;
+//   func2(x);
+//   // x = 1;
+//   console.log(x);
+// };
+
+// const func2 = y => {
+//   y += 2;
+//   func3(z);
+//   // y = 3;
+//   console.log(y);
+// };
+
+// const func3 = z => {
+//   z += 4;
+//   // z = 7;
+//   console.log(z);
+// };
+
+
+
+// func1({});
+
+// const func1 = x => {
+//   x.a = 1;
+//   func2(x); // don't need to x = func2(x)
+//   // { a: 1, b: 2, c: 4 }
+//   console.log(x);
+// };
+
+// const func2 = y => {
+//   y.b = 2;
+//   func3(y);
+//   // { a: 1, b: 2, c: 4 }
+//   console.log(y);
+// };
+
+// const func3 = z => {
+//   z.c = 4;
+//   // { a: 1, b: 2, c: 4 }
+//   console.log(z);
+// };
+
+
+const rec = (objMap, step) => {
+
+  if(objMap[step]['count'] === 1) {
+    if(step === 'end') {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  objMap[step]['count'] += 1;
+
+  const nextSteps = Object.keys(objMap[step]);
+  let roadNum = 0;
+  nextSteps.forEach(ele => {
+    if(ele !== 'count') {
+      roadNum += rec(objMap, ele);
+    }
+  });
+
+  if(step[0] === step[0].toLowerCase()) {
+    objMap[step]['count'] = 0;
+  }
+
+  return roadNum;
+
+};
+
+const day12Part1 = input => {
+
+  const objMap = {};
+  input.forEach(str => {
+    for(let i = 1; i < str.length; i++) {
+      if(str[i] === '-') {
+        if(objMap[str.substring(0, i)] === undefined) {
+          objMap[str.substring(0, i)] = {};
+          objMap[str.substring(0, i)]['count'] = 
+          str.substring(0, i)[0] === str.substring(0, i)[0].toLowerCase() ? 
+          0 : 'unlimited';
+        }
+        if(objMap[str.substring(i + 1)] === undefined) {
+          objMap[str.substring(i + 1)] = {};
+          objMap[str.substring(i + 1)]['count'] = 
+          str.substring(i + 1)[0] === str.substring(i + 1)[0].toLowerCase() ? 
+          0 : 'unlimited';
+        } 
+        objMap[str.substring(0, i)][str.substring(i + 1)] = {};
+        objMap[str.substring(i + 1)][str.substring(0, i)] = {};
+        break;
+      }
+    }
+  });
+  objMap['end']['count'] = 1;
+
+  return rec(objMap, 'start');
+
+};
+
+console.log(day12Part1(test));
+console.log(day12Part1(real));
---
+++ Advent/Day12/input.js
@@ -0,0 +1,46 @@
+
+const test = 
+[
+    'dc-end',
+    'HN-start',
+    'start-kj',
+    'dc-start',
+    'dc-HN',
+    'LN-dc',
+    'HN-end',
+    'kj-sa',
+    'kj-HN',
+    'kj-dc'
+];
+
+const real = 
+[
+    'bm-XY',
+    'ol-JS',
+    'bm-im',
+    'RD-ol',
+    'bm-QI',
+    'JS-ja',
+    'im-gq',
+    'end-im',
+    'ja-ol',
+    'JS-gq',
+    'bm-AF',
+    'RD-start',
+    'RD-ja',
+    'start-ol',
+    'cj-bm',
+    'start-JS',
+    'AF-ol',
+    'end-QI',
+    'QI-gq',
+    'ja-gq',
+    'end-AF',
+    'im-QI',
+    'bm-gq',
+    'ja-QI',
+    'gq-RD'
+];
+
+
+module.exports = { test, real };
