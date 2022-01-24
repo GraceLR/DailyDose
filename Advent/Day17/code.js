d6e4e79f42b51e6ec5c348f4de05d3be28a52db8
GraceLR <gracerwork08@gmail.com>
2022-01-24 22:42:48 +0000

    advent day17 part2

--- Advent/Day17/code.js
+++ Advent/Day17/code.js
@@ -1,17 +1,24 @@
 
 const { test, real } = require("./input");
 
-const day17part1 = (vxL, vxR, vyL, vyR) => {
+const resCt = obj => {
+  const xKeys = Object.keys(obj);
+  let resCt = 0;
+  xKeys.forEach(x => {
+    const yKeys = Object.keys(obj[x]);
+    resCt += yKeys.length;
+  });
+  return resCt;
+};
+
+const day17part2 = (vxL, vxR, vyL, vyR) => {
 
   let vy;
   let n;
   let vx;
-  let res = {
-    res1: 0,
-    res2: 0
-  };
+  let res = {};
 
-  for(let vx = 1; vx < vxR; vx++) {
+  for(let vx = 1; vx <= vxR; vx++) {
     for(let vy = 1; vy < vyR; vy++) {
       for(let n = 1; n <= Number.MAX_VALUE; n++) {
 
@@ -19,25 +26,57 @@ const day17part1 = (vxL, vxR, vyL, vyR) => {
         let y = nTotal * n / 2;
         let x1 = (2 * vx - nTotal + 1) * nTotal / 2;
         let x2 = vx * (vx + 1) / 2;
-        let height = ((vy + 1) * vy) / 2;
 
         if(y > vyR) {
           break;
         }
         if((y >= vyL && y <= vyR) && (vx >= nTotal) && 
           (x1 >= vxL && x1 <= vxR && x1 <= x2)) {
-          res.res1 = height > res.res1 ? height : res.res1;
+            if(res[vx] === undefined) {
+              res[vx] = {};
+            }
+            res[vx][vy] = true;
         }
         if((y >= vyL && y <= vyR) && (vx < nTotal) && 
           (x2 >= vxL && x2 <= vxR)) {
-          res.res2 = height > res.res2 ? height : res.res2;
+            if(res[vx] === undefined) {
+              res[vx] = {};
+            }
+            res[vx][vy] = true;
+        }
+      }
+    }
+    for(let vy = -vyR; vy <= 0; vy++) {
+      for(let n = 1; n <= Number.MAX_VALUE; n++) {
+
+        let nTotal = n;
+        let y = ((-2) * vy + n - 1) * n / 2;
+        let x1 = (2 * vx - nTotal + 1) * nTotal / 2;
+        let x2 = vx * (vx + 1) / 2;
+
+        if(y > vyR) {
+          break;
+        }
+        if((y >= vyL && y <= vyR) && (vx >= nTotal) && 
+          (x1 >= vxL && x1 <= vxR && x1 <= x2)) {
+            if(res[vx] === undefined) {
+              res[vx] = {};
+            }
+            res[vx][vy] = true;
+        }
+        if((y >= vyL && y <= vyR) && (vx < nTotal) && 
+          (x2 >= vxL && x2 <= vxR)) {
+            if(res[vx] === undefined) {
+              res[vx] = {};
+            }
+            res[vx][vy] = true;
         }
       }
     }
   }
 
-  return res;
+  return resCt(res);
 
 };
 
-console.log(day17part1(253,280,46,73));
+console.log(day17part2(253,280,46,73));
