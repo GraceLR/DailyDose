4f046b913c3c50bd88cce4fac0534ce44c3e4723
GraceLR <gracerwork08@gmail.com>
2022-02-17 00:03:58 +0000

    turn into object

--- LeetCode/findMaximumXOR.js
+++ LeetCode/findMaximumXOR.js
@@ -1,10 +1,34 @@
 
+
 const findMaximumXOR = nums => {
-    const bin = 
+
+    let objBin = {};
+
+    for(let i = 0; i < nums.length; i++) {
+
+        const num = nums[i];
+        const bin = num.toString(2);
+        const binLen = bin.length;
+
+        for(let j = binLen - 1; j >= 0; j--) {
+            if(objBin[binLen - 1 - j] === undefined) {
+                objBin[binLen - 1 - j] = {};
+            }
+            if(objBin[binLen - 1 - j][bin[j]] === undefined) {
+                objBin[binLen - 1 - j][bin[j]] = {};
+            }
+            objBin[binLen - 1 - j][bin[j]][i] = true;
+        }
+
+    }
+
+    const binPos = Object.keys(objBin).sort((a, b) => b - a);
+    const maxLenBins = objBin[binPos[0]][[1]];
+    let res = 0;
+
+
+    return res;
 };
 
-00000000
-00008421
 
-num % 2 = 1; (num - 1)/2
-num % 2 = 0; num/2
+console.log(findMaximumXOR([3,10,5,25,2,8]))
