a374e638f1854fa25dd4713f6dd589933d565032
GraceLR <gracerwork08@gmail.com>
2022-02-17 20:41:54 +0000

    first version

--- LeetCode/findMaximumXOR.js
+++ LeetCode/findMaximumXOR.js
@@ -2,33 +2,72 @@
 
 const findMaximumXOR = nums => {
 
-    let objBin = {};
+  const numsSorted = nums.sort((a, b) => b - a);
+  let objBin = {};
 
-    for(let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < numsSorted.length; i++) {
 
-        const num = nums[i];
-        const bin = num.toString(2);
-        const binLen = bin.length;
+    const num = numsSorted[i];
+    const bin = num.toString(2);
+    const binLen = bin.length;
 
-        for(let j = binLen - 1; j >= 0; j--) {
-            if(objBin[binLen - 1 - j] === undefined) {
-                objBin[binLen - 1 - j] = {};
-            }
-            if(objBin[binLen - 1 - j][bin[j]] === undefined) {
-                objBin[binLen - 1 - j][bin[j]] = {};
-            }
-            objBin[binLen - 1 - j][bin[j]][i] = true;
-        }
-
+    for (let j = binLen - 1; j >= 0; j--) {
+      if (objBin[binLen - 1 - j] === undefined) {
+        objBin[binLen - 1 - j] = {};
+      }
+      if (objBin[binLen - 1 - j][bin[j]] === undefined) {
+        objBin[binLen - 1 - j][bin[j]] = {};
+      }
+      objBin[binLen - 1 - j][bin[j]][i] = true;
     }
 
-    const binPos = Object.keys(objBin).sort((a, b) => b - a);
-    const maxLenBins = objBin[binPos[0]][[1]];
-    let res = 0;
+  }
 
+  const binPos = Object.keys(objBin).sort((a, b) => b - a);
+  const maxLenBins = Object.keys(objBin[binPos[0]][[1]]);
 
-    return res;
+  const rec = (ele, arr, pos) => {
+
+    if (arr.length === 1) {
+      return arr;
+    }
+    if (pos === -1) {
+      return arr;
+    }
+
+    for (let j = 0; j <= 1; j++) {
+      if (objBin[pos][j] === undefined) {
+        return rec(ele, arr, pos - 1);
+      } else if (objBin[pos][j][ele] === undefined) {
+        let nextArr = [];
+        for (let i = 0; i < arr.length; i++) {
+          if (objBin[pos][j][arr[i]]) {
+            nextArr.push(arr[i]);
+          }
+        }
+        if (nextArr.length === 0) {
+          return rec(ele, arr, pos - 1);
+        } else {
+          return rec(ele, nextArr, pos - 1);
+        }
+      }
+    }
+  };
+
+  let initArr = [];
+  for (let i = Number(maxLenBins[maxLenBins.length - 1]) + 1; i < nums.length; i++) {
+    initArr.push(i);
+  }
+
+  let res = 0;
+  maxLenBins.forEach(ele => {
+    const posArr = rec(ele, initArr, Number(binPos[0]) - 1);
+    const sum = numsSorted[ele] ^ numsSorted[posArr[0]];
+    res = sum > res ? sum : res;
+  });
+
+  return res;
 };
 
 
-console.log(findMaximumXOR([3,10,5,25,2,8]))
+console.log(findMaximumXOR([14,70,53,83,49,91,36,80,92,51,66,70]));
