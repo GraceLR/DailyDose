90481d85e2fa704adb1ad480f03dae561888a026
GraceLR <gracerwork08@gmail.com>
2022-02-28 17:00:01 +0000

    version 3 fixed

--- LeetCode/originalDigits.js
+++ LeetCode/originalDigits.js
@@ -1,18 +1,18 @@
 
 let originalDigits = s => {
 
-    const numArr = 
+  const numArr =
     [
-        { num: 0, arr: ['z', 'e', 'r', 'o'] },
-        { num: 2, arr: ['w', 't', 'o'] },
-        { num: 4, arr: ['u', 'f', 'r', 'o'] },
-        { num: 6, arr: ['x', 'i', 's'] },
-        { num: 8, arr: ['g', 'e', 'i', 'h', 't'] },
-        { num: 1, arr: ['o', 'n', 'e'] },
-        { num: 3, arr: ['t', 'h', 'r', 'e', 'e'] },
-        { num: 5, arr: ['f', 'i', 'v', 'e'] },
-        { num: 7, arr: ['s', 'e', 'v', 'e', 'n'] },
-        { num: 9, arr: ['i', 'n', 'n', 'e'] }
+      { num: 0, arr: ['z', 'e', 'r', 'o'] },
+      { num: 2, arr: ['w', 't', 'o'] },
+      { num: 4, arr: ['u', 'f', 'r', 'o'] },
+      { num: 6, arr: ['x', 'i', 's'] },
+      { num: 8, arr: ['g', 'e', 'i', 'h', 't'] },
+      { num: 1, arr: ['o', 'n', 'e'] },
+      { num: 3, arr: ['t', 'h', 'r', 'e', 'e'] },
+      { num: 5, arr: ['f', 'i', 'v', 'e'] },
+      { num: 7, arr: ['s', 'e', 'v', 'e', 'n'] },
+      { num: 9, arr: ['i', 'n', 'n', 'e'] }
     ];
 
   let sObj = {};
@@ -26,25 +26,29 @@ let originalDigits = s => {
 
     const obj = numArr[i];
     const arr = obj['arr'];
-    const ct = sObj[arr[0]]
-    if(ct !== undefined) {
+    const ct = sObj[arr[0]];
+    if (ct !== undefined) {
+        
+      for (let j = 1; j <= ct; j++) {
         res.push(obj['num']);
-        arr.forEach(l => {
-            sObj[l] -= ct;
-            if(sObj[l] === 0) {
-                delete sObj[l];
-            }
-        });
+      }
+
+      arr.forEach(l => {
+        sObj[l] -= ct;
+        if (sObj[l] === 0) {
+          delete sObj[l];
+        }
+      });
     }
 
     if (Object.keys(sObj).length === 0) {
-        break;
-      }
+      break;
+    }
 
   }
 
   return res.sort((a, b) => a - b).join('');
-  
+
 };
 
-console.log(originalDigits("zeroonetwothreefourfivesixseveneightnine"));
+console.log(originalDigits("zerozeor"));
