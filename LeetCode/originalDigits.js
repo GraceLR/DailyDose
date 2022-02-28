92284af88f24d6ff078b1211171ff3d2591925f7
GraceLR <gracerwork08@gmail.com>
2022-02-28 16:56:25 +0000

    version 3

--- LeetCode/originalDigits.js
+++ LeetCode/originalDigits.js
@@ -1,18 +1,18 @@
 
 let originalDigits = s => {
 
-  const numArr =
+    const numArr = 
     [
-      { z: 1, e: 1, r: 1, o: 1},
-      { o: 1, n: 1, e: 1 },
-      { t: 1, w: 1, o: 1 },
-      { t: 1, h: 1, r: 1, e: 2 },
-      { f: 1, o: 1, u: 1, r: 1 },
-      { f: 1, i: 1, v: 1, e: 1 },
-      { s: 1, i: 1, x: 1 },
-      { s: 1, e: 2, v: 1, n: 1 },
-      { e: 1, i: 1, g: 1, h: 1, t: 1 },
-      { n: 2, i: 1, e: 1 }
+        { num: 0, arr: ['z', 'e', 'r', 'o'] },
+        { num: 2, arr: ['w', 't', 'o'] },
+        { num: 4, arr: ['u', 'f', 'r', 'o'] },
+        { num: 6, arr: ['x', 'i', 's'] },
+        { num: 8, arr: ['g', 'e', 'i', 'h', 't'] },
+        { num: 1, arr: ['o', 'n', 'e'] },
+        { num: 3, arr: ['t', 'h', 'r', 'e', 'e'] },
+        { num: 5, arr: ['f', 'i', 'v', 'e'] },
+        { num: 7, arr: ['s', 'e', 'v', 'e', 'n'] },
+        { num: 9, arr: ['i', 'n', 'n', 'e'] }
     ];
 
   let sObj = {};
@@ -20,33 +20,21 @@ let originalDigits = s => {
     sObj[ele] = sObj[ele] === undefined ? 1 : sObj[ele] + 1;
   });
 
-  let res = '';
+  let res = [];
 
   for (let i = 0; i < 10; i++) {
 
-    const num = numArr[i];
-    const letterArr = Object.keys(num);
-    let cond = true;
-
-    while (cond) {
-
-      for (let j = 0; j < letterArr.length; j++) {
-        const l = letterArr[j];
-        if (sObj[l] === undefined || sObj[l] < num[l]) {
-          cond = false;
-          break;
-        }
-      }
-      if (cond) {
-        res += i;
-        letterArr.forEach(l => {
-          sObj[l] -= num[l];
-          if (sObj[l] === 0) {
-            delete sObj[l];
-          }
+    const obj = numArr[i];
+    const arr = obj['arr'];
+    const ct = sObj[arr[0]]
+    if(ct !== undefined) {
+        res.push(obj['num']);
+        arr.forEach(l => {
+            sObj[l] -= ct;
+            if(sObj[l] === 0) {
+                delete sObj[l];
+            }
         });
-      }
-
     }
 
     if (Object.keys(sObj).length === 0) {
@@ -55,7 +43,8 @@ let originalDigits = s => {
 
   }
 
-  return res;
+  return res.sort((a, b) => a - b).join('');
+  
 };
 
-console.log(originalDigits("zeroonetwothreefurfivesixsev"));
+console.log(originalDigits("zeroonetwothreefourfivesixseveneightnine"));
