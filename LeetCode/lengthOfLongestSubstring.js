a796f1814b77a5ddb7de54a0ade9f0f483aabc38
GraceLR <gracerwork08@gmail.com>
2022-03-02 16:32:06 +0000

    sliding window

--- LeetCode/lengthOfLongestSubstring.js
+++ LeetCode/lengthOfLongestSubstring.js
@@ -1,24 +1,39 @@
 
 
+// sliding window
+
 const lengthOfLongestSubstring = s => {
 
+    let left = 0;
+    let obj = {};
     let maxLen = 0;
     let len = 0;
-    let obj = {};
 
     for(let i = 0; i < s.length; i++) {
 
         const l = s[i];
+
         if(obj[l] === undefined) {
-            len ++;
-        } else {
-            maxLen = len > maxLen ? len : maxLen;
+
+            len = i - left + 1;
+
+        } else if(obj[l] >= left) {
+            
+            len = i - left;
+            left = obj[l] + 1;
         }
-        obj[l] = 1;
+
+        obj[l] = i;
+        maxLen = len > maxLen ? len : maxLen;
+
     }
 
-    return Math.max(len, maxLen);
+    return maxLen;
 
 };
 
-console.log(lengthOfLongestSubstring('abcabcdbb'))
+
+console.log(lengthOfLongestSubstring('dvdf'))
+console.log(lengthOfLongestSubstring('pwwkew'))
+console.log(lengthOfLongestSubstring('abcabcbf'))
+console.log(lengthOfLongestSubstring("ckilbkd"))
