ab92ef39341e71d3d6b799b2115abf013520ebd9
GraceLR <gracerwork08@gmail.com>
2022-03-23 00:41:35 +0000

    version 2

--- LeetCode/addTwoNumbers.js
+++ LeetCode/addTwoNumbers.js
@@ -1,10 +1,17 @@
 
 
-const nodes = input => {
+const nodes = (input, zeroNum) => {
 
     let obj = {};
     let next = obj;
     const len = input.length;
+
+    for (let i = 0; i < zeroNum; i++) {
+
+        next[0] = {};
+        next = next[0];
+
+    }
 
     for (let i = 0; i < len; i++) {
 
@@ -28,8 +35,11 @@ const nodes = input => {
 
 const addTwoNumbers = (l1, l2) => {
 
-    const obj1 = nodes(l1);
-    const obj2 = nodes(l2);
+    const l1Len = l1.length;
+    const l2Len = l2.length;
+    const len = Math.max(l1Len, l2Len);
+    const obj1 = nodes(l1, len - l1Len);
+    const obj2 = nodes(l2, len - l2Len);
     const res = [];
 
     const rec = (next1, next2) => {
@@ -49,15 +59,21 @@ const addTwoNumbers = (l1, l2) => {
     
         const carry = rec(next1, next2);
     
-        res.push((Number(key1) + Number(key2) + carry) % 10)
+        res.push((Number(key1) + Number(key2) + carry) % 10);
     
         return Math.floor((Number(key1) + Number(key2) + carry) / 10);
     
     };
 
-    rec(obj1, obj2);
+    const carry = rec(obj1, obj2);
+
+    if (carry === 1) {
+
+        res.push(1);
+        
+    }
 
     return res;
 };
 
-console.log(addTwoNumbers([2,4,3], [5,6,4]));
+console.log(addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]));
