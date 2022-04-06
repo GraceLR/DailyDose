7e516889bd0db80cfe0dc99b80e707d2cc73cf17
GraceLR <gracerwork08@gmail.com>
2022-04-06 22:21:40 +0000

    eslint

--- LeetCode/reverse.js
+++ LeetCode/reverse.js
@@ -3,40 +3,40 @@
 
 const reverse = x => {
 
-    let y = x;
+  let y = x;
 
-    if (y < 0) {
+  if (y < 0) {
 
-        y = -y;
+    y = -y;
 
-    }
+  }
 
-    const maxInt = (2 ** 31) - 1;
-    const maxIntModu = maxInt % 10;
+  const maxInt = (2 ** 31) - 1;
+  const maxIntModu = maxInt % 10;
 
-    let res = 0;
+  let res = 0;
 
-    while (y >= 10 || y <= -10) {
+  while (y >= 10 || y <= -10) {
 
-        const modu = y % 10;
+    const modu = y % 10;
     
-        res = res * 10 + modu;
+    res = res * 10 + modu;
 
-        if (res > maxInt / 10 ||
+    if (res > maxInt / 10 ||
             (res >= (maxInt - maxIntModu) / 10 &&
             modu >= maxIntModu + 1)) {
 
-            return 0;
+      return 0;
 
-        }
-
-        y = Math.floor(y / 10);
-    
     }
 
-    res = res * 10 + y;
+    y = Math.floor(y / 10);
+    
+  }
 
-    return x < 0 ? -res : res;
+  res = res * 10 + y;
+
+  return x < 0 ? -res : res;
 
 };
 
