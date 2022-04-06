d885ac5632950b51f1eea708f117719b9373345d
GraceLR <gracerwork08@gmail.com>
2022-04-06 20:41:01 +0000

    version 2 -- overflow dealt with

--- LeetCode/reverse.js
+++ LeetCode/reverse.js
@@ -1,4 +1,6 @@
 
+// can not overflow at anytime
+
 const reverse = x => {
 
     let y = x;
@@ -9,24 +11,34 @@ const reverse = x => {
 
     }
 
+    const maxInt = (2 ** 31) - 1;
+    const maxIntModu = maxInt % 10;
+
     let res = 0;
 
     while (y >= 10 || y <= -10) {
+
+        const modu = y % 10;
     
-        res = res * 10 + (y % 10);
-    
+        res = res * 10 + modu;
+
+        if (res > maxInt / 10 ||
+            (res >= (maxInt - maxIntModu) / 10 &&
+            modu >= maxIntModu + 1)) {
+
+            return 0;
+
+        }
+
         y = Math.floor(y / 10);
     
     }
 
-    res = res * 10 + y
-
-    res = res > (2 ** 31) - 1 || res < (-2) ** 31 ? 0 : res;
-    // if we can tell in the first step, would be faset
+    res = res * 10 + y;
 
     return x < 0 ? -res : res;
 
 };
 
 
-console.log(reverse(-123));
+console.log(reverse(15342364));
