86a3211b6b674bcc52e2c86218bd0168cf74e9f5
GraceLR <gracerwork08@gmail.com>
2022-04-11 18:36:54 +0000

    first try success

---
+++ LeetCode/maxArea.js
@@ -0,0 +1,39 @@
+
+const maxArea = height => {
+
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+
+        let h = undefined;
+        const w = right - left;
+
+        if (height[left] <= height[right]) {
+
+            h = height[left];
+            left++;
+
+        } else {
+
+            h = height[right];
+            right--;
+
+        }
+
+        const area = h * w;
+
+        if (area > maxArea) {
+
+            maxArea = area;
+
+        }
+
+    }
+
+    return maxArea;
+
+};
+
+console.log(maxArea([1,2,15]))
