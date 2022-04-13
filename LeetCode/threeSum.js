81745392fb416b2e32865f08aa01290d4f08da0a
GraceLR <gracerwork08@gmail.com>
2022-04-13 18:39:34 +0000

    use twoSum Runtime: 136 ms, faster than 88.90% of JavaScript, Memory Usage: 52.5 MB, less than 73.02% of JavaScript

--- LeetCode/threeSum.js
+++ LeetCode/threeSum.js
@@ -1,49 +1,123 @@
 
+// const threeSum = nums => {
+
+//   const numsSorted = nums.sort((a, b) => a - b);
+//   const len = nums.length;
+
+//   let obj = {};
+
+//   for (let i = 0; i < len; i++) {
+
+//     obj[numsSorted[i]] = i;
+
+//   }
+
+//   let res = [];
+
+//   for (let i = 0; i < len; i++) {
+
+//     if (numsSorted[i] > 0) {
+
+//         break;
+
+//     }
+
+//     while (i > 0 && numsSorted[i] === numsSorted[i - 1]) {
+
+//         if (i === len - 1) {
+
+//             return res;
+
+//         }
+
+//       i++;
+
+//     }
+
+//     const numi = numsSorted[i];
+
+//     for (let j = i + 1; j < len; j++) {
+
+//       while (j > i + 1 && numsSorted[j] === numsSorted[j - 1]) {
+
+//         j++;
+    
+//       }
+
+//       const numj = numsSorted[j];
+//       const rem = 0 - (numi + numj);
+//       const pos = obj[rem];
+
+//       if (pos !== undefined && pos > j) {
+
+//         res.push([numi, numj, rem]);
+
+//       }
+
+//     }
+
+//   }
+
+//   return res;
+
+// };
+
+
+// twoSum on after i
+
 const threeSum = nums => {
 
   const numsSorted = nums.sort((a, b) => a - b);
-
-  let obj = {};
-
-  for (let i = 0; i < nums.length; i++) {
-
-    obj[numsSorted[i]] = i;
-
-  }
-
   let res = [];
+  const len = numsSorted.length;
 
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < len; i++) {
 
     if (numsSorted[i] > 0) {
 
-        break;
+      break;
 
     }
 
     while (i > 0 && numsSorted[i] === numsSorted[i - 1]) {
 
+      if (i === len - 1) {
+
+        return res;
+
+      }
+
       i++;
 
     }
 
-    const numi = numsSorted[i];
+    const num = numsSorted[i];
+    const rem = -num;
+    let left = i + 1;
+    let right = len - 1;
+        
+    while (left < right) {
 
-    for (let j = i + 1; j < nums.length; j++) {
+      const sum = numsSorted[left] + numsSorted[right];
 
-      while (j > i + 1 && numsSorted[j] === numsSorted[j - 1]) {
+      if (sum > rem) {
 
-        j++;
-    
-      }
+        right--;
 
-      const numj = numsSorted[j];
-      const rem = 0 - (numi + numj);
-      const pos = obj[rem];
+      } else if (sum < rem) {
 
-      if (pos !== undefined && pos > j) {
+        left++;
 
-        res.push([numi, numj, rem]);
+      } else {
+
+        res.push([num, numsSorted[left], numsSorted[right]]);
+        left++;
+
+        while (numsSorted[left] === numsSorted[left - 1]) {
+
+          left++;
+
+        }
 
       }
 
@@ -55,73 +129,4 @@ const threeSum = nums => {
 
 };
 
-
-// twoSum on after i
-
-// const threeSum = nums => {
-
-//     const numsSorted = nums.sort((a, b) => a - b);
-
-//     const twoSum = (pos, rem) => {
-
-//         let obj = {};
-//         let res = [];
-
-//         for (let i = pos; i < numsSorted.length; i++) {
-
-//             const num = numsSorted[i];
-//             const num2 = rem - num; 
-
-//             if (obj[num2] && !obj[num]) {
-    
-//                 res.push([num2, num]);
-
-//             }
-
-//             obj[num] = i;
-
-//         }
-
-//         return res;
-
-//     };
-
-//     let res = [];
-//     const len = numsSorted.length;
-
-//     for (let i = 0; i < len; i++) {
-
-//         while (i > 0 && numsSorted[i] === numsSorted[i - 1]) {
-
-//             i++;
-
-//             if (i === len) {
-
-//                 return res;
-
-//             }
-
-//         }
-
-//         const num = numsSorted[i];
-//         const twoSumRes = twoSum(i + 1, -num);
-//         const twoSumResLen = twoSumRes.length;
-
-//         if (twoSumResLen > 0) {
-
-//           twoSumRes.forEach(arr => {
-
-//             arr.push(num);
-//             res.push(arr);
-
-//           });
-
-//         }
-
-//     }
-
-//     return res;
-
-// };
-
-console.log(threeSum([0,0,0,0]));
+console.log(threeSum([-1,0,1,2,-1,-4]));
