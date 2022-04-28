23543a388e06504b4605d42d53c181b80aa78ae6
GraceLR <gracerwork08@gmail.com>
2022-04-28 02:03:41 +0000

    draft

--- LeetCode/threeSumClosest.js
+++ LeetCode/threeSumClosest.js
@@ -1,2 +1,50 @@
 
+const threeSumClosest = (nums, target) => {
 
+    const numsSorted = nums.sort((a, b) => a - b);
+    const len = nums.length;
+    let dis = Number.MAX_VALUE; 
+    let res = 0;
+
+    for (let i = 0; i < len; i++) {
+
+        const num1 = numsSorted[i];
+
+        for (let j = i + 1; j < len; j++) {
+
+            const num2 = numsSorted[j];
+            let disLocal = Number.MAX_VALUE;
+
+            for (let k = j + 1; k < len; k++) {
+
+                const num3 = numsSorted[k];
+                const sum = num1 + num2 + num3;
+                const curDis = Math.abs(sum - target);
+
+                if (curDis <= disLocal) {
+
+                    disLocal = curDis;
+
+                    if (curDis < dis) {
+
+                        dis = curDis;
+                        res = sum;
+
+                    }
+
+                } else {
+
+                    break;
+
+                }
+
+
+            }
+
+        }
+
+    }
+
+    return res;
+    
+};
