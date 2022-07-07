3836e74788f1a25f86dd788777a51c9c474e5e7a
GraceLR <gracerwork08@gmail.com>
2022-07-07 14:34:13 -0400

    version two

--- LeetCode/removeDuplicates.js
+++ LeetCode/removeDuplicates.js
@@ -1,17 +1,32 @@
+// const removeDuplicates = nums => {
+//     let k = 1;
+//     const len = nums.length;
+//     let ct = 1;
+//     let i = 1;
+//     while (ct < len) {
+//         if (nums[i] === nums[i - 1]) {
+//             const rep = nums.splice(i, 1);
+//             nums.push(rep);
+//         } else{
+//             i++;
+//             k++;
+//         }
+//         ct++;
+//     }
+//     return k;
+// };
+
 const removeDuplicates = nums => {
-    let k = 1;
     const len = nums.length;
-    let ct = 1;
-    let i = 1;
-    while (ct < len) {
-        if (nums[i] === nums[i - 1]) {
-            const rep = nums.splice(i, 1);
-            nums.push(rep);
-        } else{
-            i++;
-            k++;
-        }
-        ct++;
+    if (len === 0) {
+        return 0;
     }
-    return k;
+    let i = 0;
+    for (let j = 1; j < len; j++) {
+        if (nums[j] !== nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
 };
