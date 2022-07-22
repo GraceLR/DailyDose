9520ca058cafd5e173bfc0826d74ff6b906bfa11
GraceLR <gracerwork08@gmail.com>
2022-07-22 11:22:22 -0400

    for leetcode max_int boundry

--- LeetCode/divide.js
+++ LeetCode/divide.js
@@ -22,11 +22,24 @@ const rec = (num, residual) => {
     return (2 ** n) + rec(num, residual - (2 ** n) * num);
 };
 
+// const divide = (dividend, divisor) => {
+//     let residual = absolute(dividend);
+//     let absDivisor = absolute(divisor);
+//     const res = rec(absDivisor, residual);
+//     return dividend * divisor < 0 ? -res : res;
+// };
+
 const divide = (dividend, divisor) => {
     let residual = absolute(dividend);
     let absDivisor = absolute(divisor);
-    const res = rec(absDivisor, residual);
-    return dividend * divisor < 0 ? -res : res;
+    let res = rec(absDivisor, residual);
+    res = dividend * divisor < 0 ? -res : res;
+        if (res > 2147483647) {
+        res = 2147483647;
+    } else if (res < -2147483648) {
+        res = -2147483648;
+    }
+    return res;
 };
 
 console.log(divide(-2147483648, 5));
