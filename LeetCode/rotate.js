df3dd20df9f47856805883e3556874293493f1e8
GraceLR <gracerwork08@gmail.com>
2022-09-17 18:03:02 -0400

    version 2

--- LeetCode/rotate.js
+++ LeetCode/rotate.js
@@ -1,4 +1,4 @@
-const rotate = (matrix) => {
+const rotate_v1 = (matrix) => {
   const len = matrix.length;
   for (let i = 0; i < len; i++) {
     matrix.unshift([]);
@@ -11,5 +11,31 @@ const rotate = (matrix) => {
   }
   for (let i = newLen - 1; i >= len; i--) {}
   matrix.splice(len, len);
+  return matrix;
+};
+
+const rotate_v2 = (matrix) => {
+  const len = matrix.length;
+  const n = Math.floor(len / 2);
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < len - 1 - i; j++) {
+      let ct = 0;
+      let prev = matrix[i][j];
+      let nexti = j;
+      let nextj = len - 1 - i;
+      while (ct < 4) {
+        const current = matrix[nexti][nextj];
+        matrix[nexti][nextj] = prev;
+        prev = current;
+        if (ct === 3) {
+          break;
+        }
+        const jsave = nextj;
+        nextj = len - 1 - nexti;
+        nexti = jsave;
+        ct++;
+      }
+    }
+  }
   return matrix;
 };
