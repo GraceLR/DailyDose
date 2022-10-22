caa2c43b902b2228a79c0ddac1457f09b72a9115
GraceLR <gracerwork08@gmail.com>
2022-10-21 20:21:41 -0400

    combinationSum2 wip

---
+++ LeetCode/combinationSum2.js
@@ -0,0 +1,63 @@
+var combinationSum2 = function (candidates, target) {
+  const sortedCdd = candidates.sort((a, b) => b - a);
+  const len = candidates.length;
+  const result = [];
+  const binSearch = (pos, target) => {
+    if (pos === len - 1 && sortedCdd[pos] === target) {
+      return { found: true, pos: pos + 1 };
+    }
+    let left = pos;
+    let right = len - 1;
+    let mid = Math.floor((pos + len - 1) / 2);
+    while (left < right) {
+      if (sortedCdd[left] === target) {
+        return { found: true, pos: left + 1 };
+      }
+      if (sortedCdd[right === target]) {
+        return { found: true, pos: right + 1 };
+      }
+      const numMid = sortedCdd[mid];
+      if (numMid === target) {
+        return { found: true, pos: mid + 1 };
+      } else if (numMid < target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return { found: false, pos: mid + 1 };
+  };
+  const rec = (pos, target, resArr) => {
+    if (sortedCdd[len - 1] > target) {
+      return;
+    }
+    for (let i = pos; i < len; i++) {
+      const num = sortedCdd[i];
+      if (!(num > target || (i > 0 && num === sortedCdd[i - 1]))) {
+        const newResArr = [...resArr, num];
+        const newTarget = target - num;
+        const res = binSearch(i + 1, newTarget);
+        let newPos;
+        if (res.found) {
+          result.push([...newResArr, sortedCdd[res.pos - 1]]);
+          for (let i = res.pos; i < len; i++) {
+            if (sortedCdd[i] !== newTarget) {
+              newPos = i;
+              break;
+            }
+          }
+        } else {
+          newPos = res.pos;
+        }
+        if (res.pos !== undefined) {
+          rec(newPos, newTarget, newResArr);
+        }
+      }
+    }
+    return;
+  };
+  rec(0, target, []);
+  return result;
+};
+
+console.log(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8));
