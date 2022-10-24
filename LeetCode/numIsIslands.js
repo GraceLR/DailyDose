507c5ef2cc69c996c9480e02c1408854f65db838
GraceLR <gracerwork08@gmail.com>
2022-10-24 14:44:29 -0400

    wip

--- LeetCode/numIsIslands.js
+++ LeetCode/numIsIslands.js
@@ -1,13 +1,17 @@
 const numIslands = (grid) => {
   const gridMark = grid.map((arr) => arr.map((ele) => "0"));
-  const count = 0;
+  let count = 0;
   const rec = (i, j) => {
-    gridMark[i][j] === "1";
+    gridMark[i][j] = "1";
     if (grid[i][j + 1] === "1" && gridMark[i][j + 1] === "0") {
       rec(i, j + 1);
     } else if (grid[i][j - 1] === "1" && gridMark[i][j - 1] === "0") {
       rec(i, j - 1);
-    } else if (grid[i + 1][j] === "1" && gridMark[i + 1][j] === "0") {
+    } else if (
+      grid[i + 1] !== undefined &&
+      grid[i + 1][j] === "1" &&
+      gridMark[i + 1][j] === "0"
+    ) {
       rec(i + 1, j);
     } else if (grid[i - 1][j] === "1" && gridMark[i - 1][j] === "0") {
       rec(i - 1, j);
