8b1d58037da59f024122d619f33825f34e193f30
GraceLR <gracerwork08@gmail.com>
2022-11-23 15:56:41 -0500

    learning c#

---
+++ LeetCode/program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace consoleproject
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string greeting = "Hello World!";
+      Console.WriteLine(greeting);
+    }
+  }
+}
