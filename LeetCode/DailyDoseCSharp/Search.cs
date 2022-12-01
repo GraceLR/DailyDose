a4be1c867183ceb7839286a94e3fc0beb12b3be0
GraceLR <gracerwork08@gmail.com>
2022-12-01 17:07:56 -0500

    BinarySearch

---
+++ LeetCode/DailyDoseCSharp/Search.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyDoseCSharp
+{
+	public class Search
+	{
+		public static int Solution(int[] nums, int target)
+		{
+			int left = 0;
+			int right = nums.Length - 1;
+			while (left <= right)
+			{
+				int mid = (left + right) / 2;
+				int midNum = nums[mid];
+				if (midNum == target)
+				{
+					return mid;
+				}
+				if (midNum > target)
+				{
+					right = mid - 1;
+				}
+				else
+				{
+					left = mid + 1;
+				}
+			}
+			return -1;
+		}
+	}
+}
+
+
