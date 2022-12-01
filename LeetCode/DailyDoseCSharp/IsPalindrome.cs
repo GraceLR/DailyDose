71e0d4729c5994e8feec4fa4952beada4daf8e41
GraceLR <gracerwork08@gmail.com>
2022-12-01 14:35:38 -0500

    IsPalindrome

---
+++ LeetCode/DailyDoseCSharp/IsPalindrome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyDoseCSharp
+{
+	public class IsPalindrome
+	{
+		public static bool Solution(string s)
+		{
+			int i = 0;
+			int j = s.Length - 1;
+			while (i < j)
+			{
+				char iChar = s[i];
+				char jChar = s[j];
+				if (Char.IsLetterOrDigit(iChar) && Char.IsLetterOrDigit(jChar))
+				{
+					if (Char.ToLower(iChar) != Char.ToLower(jChar))
+					{
+						return false;
+					}
+					i++;
+					j--;
+				}
+				else if (!Char.IsLetterOrDigit(iChar))
+				{
+					i++;
+				}
+				else
+				{
+					j--;
+				}
+			}
+			return true;
+		}
+	}
+}
+
+
