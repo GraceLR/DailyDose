628348fc9aa7d3608336969cf00a04c64b3da665
GraceLR <gracerwork08@gmail.com>
2022-12-21 00:14:00 -0500

    mergeNums

--- LeetCode/mergeNums.js
+++ LeetCode/mergeNums.js
@@ -1,17 +1,17 @@
-var mergeNums = (nums1, m, nums2, n) => {
+const mergeNums = (nums1, m, nums2, n) => {
   let posFill = m + n - 1;
   let posCompare = m - 1;
   for (let i = n - 1; i >= 0; i--) {
     const num2 = nums2[i];
     const num1 = nums1[posCompare];
-    if (num1 < num2) {
+    if (num1 < num2 || num1 === undefined) {
       nums1[posFill] = num2;
-      posFill--;
     } else {
       nums1[posFill] = num1;
-      nums1[posCompare] = num2;
       posCompare--;
+      i++;
     }
+    posFill--;
   }
   return nums1;
 };
